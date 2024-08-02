43a41842311464a0cdd7b2c484903fe06c98043a
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-08-02 15:22:01 +0530

    upload

--- src/middlewares/fileupload.middleware.js
+++ src/middlewares/fileupload.middleware.js
@@ -1,17 +1,12 @@
 import multer from "multer";
-import fs from "fs";
-import path from "path";
 
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        const dir = './Uploads/';
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        cb(null, './uploads/');
+        
+        cb(null, './src/uploads/');
     },
     filename:(req,file,cb)=>{
-        cb(null,new Date().toISOString()+file.originalname);
+        cb(null,new Date().toISOString().replace(/:/g,'_')+file.originalname);
     },
 });
 
